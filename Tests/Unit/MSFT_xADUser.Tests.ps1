5f172de7c586dcb6fef8d87ec50fef295dab2f98
Paul Shamus <pshamus@genomichealth.com>
2019-02-19 10:48:16 -0800

    Updated Pester tests including style formatting problems

--- Tests/Unit/MSFT_xADUser.Tests.ps1
+++ Tests/Unit/MSFT_xADUser.Tests.ps1
@@ -50,8 +50,8 @@ try
             'POBox', 'City', 'State', 'PostalCode', 'Country', 'Department', 'Division', 'Company', 'Office', 'JobTitle',
             'EmailAddress', 'EmployeeID', 'EmployeeNumber', 'HomeDirectory', 'HomeDrive', 'HomePage', 'ProfilePath',
             'LogonScript', 'Notes', 'OfficePhone', 'MobilePhone', 'Fax', 'Pager', 'IPPhone', 'HomePhone','CommonName'
-        )
-        $testBooleanProperties = @('PasswordNeverExpires', 'CannotChangePassword','Enabled')
+        );
+        $testBooleanProperties = @('PasswordNeverExpires', 'CannotChangePassword', 'TrustedForDelegation', 'Enabled');
 
         #region Function Get-TargetResource
         Describe "$($Global:DSCResourceName)\Get-TargetResource" {
@@ -494,6 +494,7 @@ try
                 It 'Should throw the correct error when then object cannot be restored from recycle bin' {
                     $restoreParam = $testPresentParams.Clone()
                     $restoreParam.RestoreFromRecycleBin = $true
+                {Set-TargetResource @restoreParam} | Should Throw
 
                     $script:mockCounter = 0
 
@@ -518,6 +519,11 @@ try
             It "Throws when account is disabled and 'Password' is specified" {
                 { Assert-Parameters -Password $testCredential -Enabled $false } | Should Throw
             }
+
+            It "Does not throw when 'TrustedForDelegation' is specified" {
+                { Assert-Parameters -TrustedForDelegation $true } | Should Not Throw
+            }
+
         }
         #endregion
     }
@@ -529,5 +535,3 @@ finally
     Restore-TestEnvironment -TestEnvironment $TestEnvironment
     #endregion
 }
-
-
